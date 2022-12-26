e90cf1573b3cea40669fa01dfa38a8a7d121f56c
yagi1231 <at.sao.1231@gmail.com>
2022-12-26 20:12:45 +0900

    classBase

---
+++ resources/views/components/test/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    クラスベースのコンポートネント
+    <div>{{ $defaultMessage }}</div>
+    <div>{{ $classBaseMessage }}</div>
+    <!-- Do what you can, with what you have, where you are. - Theodore Roosevelt -->
+</div>
--- resources/views/tests/compoment-test2.blade.php
+++ resources/views/tests/compoment-test2.blade.php
@@ -4,4 +4,9 @@
     </x-slot>
 コンポーネント2
 <x-test.card title="タイトル2" content="本文2" />
+<x-test-class-base classBaseMessage="メッセージです" />
+<div class="mb-4"></div>
+  <x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更しています" />
+
+
 </x-test.app>
